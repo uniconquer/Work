0cb3e7ef8b3db2446eb572b6aa0f7c6b75f92b1f
uniconquer <blueminy87@naver.com>
2015-11-08 00:41:32 +0900

    delegate수정

--- Study/PatternTest/PatternTest/Delegation.h
+++ Study/PatternTest/PatternTest/Delegation.h
@@ -7,15 +7,30 @@ typedef void (*DelegateIntFunc)();
 struct Delegate
 {
 	void (*FuncToCall)();
-	Delegate( DelegateFunc Infunc = NULL ):FuncToCall( Infunc ){}
-	virtual void operator()(){ if(FuncToCall)(*FuncToCall)();}
+	
+	Delegate(DelegateFunc Infunc = nullptr)
+		: FuncToCall(Infunc)
+	{}
+	
+	virtual void operator()()
+	{
+		if(FuncToCall)
+			(*FuncToCall)();
+	}
 };
 
 struct DelegateInt
 {
 	void (*FuncToCall)(int);
-	DelegateInt( DelegateIntFunc Infunc = NULL ):FuncToCall( /*Infunc*/ ){}
-	virtual void operator()( int p ){ if(FuncToCall)(*FuncToCall)(p);}
+	DelegateInt(DelegateIntFunc Infunc = nullptr)
+		: FuncToCall(0)
+	{}
+
+	virtual void operator()(int p)
+	{
+		if(FuncToCall)
+			(*FuncToCall)(p);
+	}
 };
 
 
